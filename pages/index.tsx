3e4aa0a4556ca102cae916dce390e260474a5d47
Shark Tech Studios <37256358+Brynm32@users.noreply.github.com>
2020-11-21 12:24:45 -0500

    site updated with nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Shark Tech Studios LLC</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="">Hello Home</h1>
+      <h2>welcome</h2>
+      
+    </div>
+  )
+}
